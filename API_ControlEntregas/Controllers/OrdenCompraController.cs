fc8055763fc8578d65822e48c23c71f3e5ccaf0f
sarn3792 <sarn3792@gmail.com>
2017-07-17 17:55:44 -0500

    Products controller and model are finished. Purchase order was started.

---
+++ API_ControlEntregas/Controllers/OrdenCompraController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using API_ControlEntregas.Models;
+using System.Threading.Tasks;
+
+namespace API_ControlEntregas.Controllers
+{
+    [EnableCorsAttribute("*", "*", "*")]
+    public class OrdenCompraController : ApiController
+    {
+        public async Task<HttpResponseMessage> Get()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ API_ControlEntregas/Models/OrdenCompraModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace API_ControlEntregas.Models
+{
+    public class OrdenCompraModel
+    {
+        public async Task<List<OrdenCompra>> Get()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<OrdenCompra> Get(Int64 idOrdenCompra)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Insert(OrdenCompra data)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+    public class OrdenCompra
+    {
+        public Int64 idOrdenCompra;
+        public Int64 idCliente;
+        public String descripcion;
+        public DateTime creadoEn;
+
+        public OrdenCompra()
+        {
+
+        }
+    }
+}
